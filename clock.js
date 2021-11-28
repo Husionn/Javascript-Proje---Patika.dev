69f8367489d014346295d5d9f15f9c16731b6000
Husionn <87138757+Husionn@users.noreply.github.com>
2021-11-28 20:50:20 +0300

    js file

--- clock.js
+++ clock.js
@@ -1,11 +1,12 @@
 let userName = prompt("Lütfen İsminizi giriniz.")
+let userNameUpperCase = `${userName[0].toUpperCase()}${userName.slice(1).toLowerCase()}`
 
 let myName = document.querySelector(`#myName`)
-myName.innerHTML = userName
+myName.innerHTML = userNameUpperCase
 
 let myClock = document.querySelector("#myClock")
 
 let time = new Date()
 
-let finalTime = `Günün Saati: ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} `
+let finalTime = `Günün Saati: ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
 myClock.innerHTML = finalTime
